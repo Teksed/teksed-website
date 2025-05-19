8450f33d54ec0a254e1956010600f618193e3e7a
Kingsley Amankwah <kingsley.amankwah@amalitech.com>
2025-05-19 21:32:59 +0000

    main component

--- src/app/teksed.component.ts
+++ src/app/teksed.component.ts
@@ -1,9 +1,11 @@
 import { Component } from "@angular/core";
 import { RouterOutlet } from "@angular/router";
+import { FooterComponent } from "./shared/components/footer/footer.component";
+import { NavbarComponent } from "./shared/components/navbar/navbar.component";
 
 @Component({
   selector: "teksed-root",
-  imports: [RouterOutlet],
-  template: "<router-outlet />",
+  imports: [RouterOutlet, NavbarComponent, FooterComponent],
+  template: `<teksed-navbar /> <router-outlet /> <teksed-footer />`,
 })
 export class TeksedComponent {}

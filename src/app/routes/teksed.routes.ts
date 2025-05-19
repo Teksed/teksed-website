d9a30f90951955c9eb38c3d0bf10f197d068b85f
Kingsley Amankwah <kingsley.amankwah@amalitech.com>
2025-05-19 21:24:29 +0000

    refactor: add home route

--- src/app/routes/teksed.routes.ts
+++ src/app/routes/teksed.routes.ts
@@ -3,7 +3,8 @@ import { titlePrefix } from "@app/utils/utils";
 
 export const routes: Routes = [
   {
-    path: "**",
-    title: `${titlePrefix} - Page Not Found`,
+    path: "",
+    loadComponent: () => import("../pages/home/home.component").then((c) => c.HomeComponent),
+    title: `${titlePrefix} - Home Page`,
   },
 ];

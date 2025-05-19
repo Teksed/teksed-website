6e528425bfb80b6b2fe4201c99473ebcabe68880
Kingsley Amankwah <kingsley.amankwah@amalitech.com>
2025-05-19 23:10:17 +0000

    setup products page

---
+++ src/app/pages/products/products.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'teksed-products',
+  imports: [],
+  templateUrl: './products.component.html',
+  styleUrl: './products.component.css'
+})
+export class ProductsComponent {
+
+}

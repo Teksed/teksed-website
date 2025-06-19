c7804360ea9da6053ef37b39fc680d29fb3e5a09
KingsleyAmankwah <kuamiklassic149@gmail.com>
2025-06-19 15:32:07 +0000

    refactor: simplify route titles by removing titlePrefix

--- frontend/src/app/routes/teksed.routes.ts
+++ frontend/src/app/routes/teksed.routes.ts
@@ -1,27 +1,26 @@
 import { Routes } from "@angular/router";
-import { titlePrefix } from "@app/utils/utils";
 
 export const routes: Routes = [
   {
     path: "",
     loadComponent: () => import("../pages/home/home.component").then((c) => c.HomeComponent),
-    title: `${titlePrefix} - Home Page`,
+    title: `Home Page`,
   },
   {
     path: "about-us",
     loadComponent: () =>
       import("../pages/about-us/about-us.component").then((c) => c.AboutUsComponent),
-    title: `${titlePrefix} - About US`,
+    title: `About US`,
   },
   {
     path: "careers",
     loadComponent: () =>
       import("../pages/careers/careers.component").then((c) => c.CareersComponent),
-    title: `${titlePrefix} - Careers`,
+    title: `Careers`,
   },
   {
     path: "blog",
     loadComponent: () => import("../pages/blog/blog.component").then((c) => c.BlogComponent),
-    title: `${titlePrefix} - Blog`,
+    title: `Blog`,
   },
 ];

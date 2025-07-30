8be9cd5d48ff22175c015d5ab50715014c9d3e62
KingsleyAmankwah <kuamiklassic149@gmail.com>
2025-07-30 08:20:03 +0000

    feat: add clients interface

---
+++ frontend/src/app/core/interfaces/clients.interface.ts
@@ -0,0 +1,10 @@
+export interface Client {
+  name: string;
+  logo?: string;
+  description: string;
+  industry: string;
+  services: string[];
+  testimonial?: string;
+  testimonialAuthor?: string;
+  testimonialRole?: string;
+}
